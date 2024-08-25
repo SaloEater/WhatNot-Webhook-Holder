59609464e840c0a06d1e12ac0587952b552aea24
SaloEater <danil.lazarev@gmail.com>
2024-08-25 10:45:34 -0500

    fix

--- service/channel_get.go
+++ service/channel_get.go
@@ -20,7 +20,6 @@ type GetChannelResponse struct {
 }
 
 func (s *Service) GetChannel(r *GetChannelRequest) (*GetChannelResponse, error) {
-	s.CacheClear()
 	key := cache.IdToKey(r.Id)
 
 	if !s.ChannelCache.Has(key) {
